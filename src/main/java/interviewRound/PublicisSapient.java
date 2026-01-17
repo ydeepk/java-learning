63716570f1d97db576851541d3689cfe924bf79c
ydeepk <ydeepkcs@gmail.com>
2026-01-17 08:47:52 +0530

    Coding exercise - ExcelUtils

--- src/main/java/interviewRound/PublicisSapient.java
+++ src/main/java/interviewRound/PublicisSapient.java
@@ -5,12 +5,13 @@ public class PublicisSapient {
     * What is sycn and asynchonose
     * how does asynchrons help improve perfomance and reliablity of software appliaction?
     * Where is local variable and Instance variable stored in memory ?
-    * What is OOPS ?
     * Local vs instance viable
     * What is default value for local and instance local ?
+    * What is OOPS ?
     * What is Abstract and how its implemented ?
     * Checked vs unchecked exception
     * What is collection and what are they ?
+    * Difference between, List, Set, Queue and Map collections
     * explicit wait and fluent wait ? and their differences
     *
     * */
