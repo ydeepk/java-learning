0a5af966a0cce35a675f04084c480e13e64b7525
ydeepk <ydeepkcs@gmail.com>
2026-01-26 19:30:10 +0530

    Coding exercise - two pointer array - palindrome

---
+++ src/main/java/basics/arrays/IsPallindrome.java
@@ -0,0 +1,33 @@
+package basics.arrays;
+
+public class IsPallindrome {
+
+    public static void main(String[] args) {
+
+        char[] arr = {'m','a','d','a','m'} ;
+        System.out.println(isPalindrome(arr));
+    }
+
+    public static boolean isPalindrome(char[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+
+        while(left < right) {
+
+            if(arr[left] != arr[right]) {
+                return false;
+            }
+
+            System.out.println(left+" matched "+right);
+
+            left++;
+            right--;
+        }
+
+        // if made through the while loop, its a palindrome
+        return true;
+
+    }
+}
